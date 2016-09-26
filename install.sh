523ab459214da65421eff0c5b1c2b33f074655d3
Vetik Tsvetik <vetik32@yahoo.com>
2016-09-26 17:38:01 +0300

    fix mysql sed command
    it needs sudo access

--- install.sh
+++ install.sh
@@ -6,6 +6,6 @@ sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again p
 sudo apt-get install -y vim curl python-software-properties
 sudo apt-get update
 sudo apt-get -y install mysql-server
-sed -i "s/^bind-address/#bind-address/" /etc/mysql/my.cnf
+sudo sed -i "s/^bind-address/#bind-address/" /etc/mysql/my.cnf
 mysql -u root -proot -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root' WITH GRANT OPTION; FLUSH PRIVILEGES;"
 sudo /etc/init.d/mysql restart
