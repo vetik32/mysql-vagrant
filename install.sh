af76a49dd243522e282f7e7ec3db58aac9347b3a
Alex <alexdisler@gmail.com>
2016-11-06 10:15:28 -0500

    set a high max_connect_errors

--- install.sh
+++ install.sh
@@ -7,5 +7,5 @@ sudo apt-get install -y vim curl python-software-properties
 sudo apt-get update
 sudo apt-get -y install mysql-server
 sed -i "s/^bind-address/#bind-address/" /etc/mysql/my.cnf
-mysql -u root -proot -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+mysql -u root -proot -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root' WITH GRANT OPTION; FLUSH PRIVILEGES; SET GLOBAL max_connect_errors=10000;"
 sudo /etc/init.d/mysql restart
